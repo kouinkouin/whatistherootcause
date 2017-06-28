cfee02f2fc42f1319dc6707628f8518c3f6e0c78
kouinkouin <me@kouinkouin.com>
2017-06-28 22:32:03 +0200

    feat: make a basic generator

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,27 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Services\Reason\ReasonGenerator;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     *
+     * @param ReasonGenerator $reasonGenerator
+     *
+     * @return Response
      */
-    public function indexAction(Request $request)
+    public function indexAction(ReasonGenerator $reasonGenerator)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render(
+            'default/index.html.twig',
+            [
+                'reason' => $reasonGenerator->getRandomReason(),
+            ]
+        );
     }
 }
---
+++ src/AppBundle/Services/Reason/ReasonGenerator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Services\Reason;
+
+class ReasonGenerator
+{
+    private $reasons = [
+        'A beer falls on the server',
+        'Bees in the server room',
+        'Candies were missed',
+    ];
+
+    public function getRandomReason(): string
+    {
+        $a = array_rand($this->reasons, 1);
+
+        return $this->reasons[$a];
+    }
+}
